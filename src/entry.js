088b819f98492b7623c20f33f85785bd02dffd1d
kasselTrankos <alvaro.touzon@gmail.com>
2016-01-11 17:53:30 +0100

    added method to run by.js

--- src/entry.js
+++ src/entry.js
@@ -1,3 +1,8 @@
-let bodyText = 'Estamos en pruebas, using ES6';
+let bodyText = 'Estamos en pruebas, using ES6d dd, please';
 const app = document.getElementById('app');
 app.innerHTML = bodyText;
+
+/////////////removes at the en of application
+if (module.hot) {
+  module.hot.accept();
+}
--- webpack.build.js
+++ webpack.build.js
@@ -1,12 +1,27 @@
+var webpack = require('webpack');
+var path  =require('path');
+var host = 'localhost';
+var port = '3000';
 module.exports = {
   context: __dirname,
+  devtool: '#source-map',
   entry:[
+    'webpack-dev-server/client?http://' + host + ':' + port,
     './src/entry'
   ],
   output: {
-    path: 'public/assets/',
-    publicPath: '/',
+    path: path.resolve(__dirname,'public'),
+    publicPath: '/assets/',
     filename: 'main.js'
-  }
-
+  },
+  module: {
+    loaders: [
+      { test: /\.js|x$/, exclude: /(node_modules|bower_components)/, loader: 'babel?presets[]=es2015'}
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
 }
